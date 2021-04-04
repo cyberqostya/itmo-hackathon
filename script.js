6f3cc996a77ad99977951ceecc79098f8fbb365c
antonkstin <stanli9@mail.ru>
2021-04-04 21:10:05 +0300

    v7

--- script.js
+++ script.js
@@ -20,17 +20,191 @@ const participationButton = participationBlock.querySelector('.participation__bu
 const timingBlock = document.querySelector('.timing');
 const timingButton = timingBlock.querySelector('.timing__button');
 
+// Hackathons
+const hackathonsBlock = document.querySelector('.hackathons');
+
+// Popup
+const popupBlock = document.querySelector('.popup');
+  const popupForm = popupBlock.querySelector('.popup__form');
+    const popupNameField = popupForm.querySelector('.popup__field-name-js');
+    const popupSurnameField = popupForm.querySelector('.popup__field-surname-js');
+    const popupUploadApplication = popupForm.querySelector('.popup__upload-label-application-js');
+    const popupUploadCV = popupForm.querySelector('.popup__upload-label-cv-js');
+    const popupUploadVideo = popupForm.querySelector('.popup__upload-label-video-js');
+    const popupButton = popupForm.querySelector('.popup__form-button');
+
+// ==============================
+
+
+
+
+// Объявление переменных
+const errorMessages = {
+  required: 'Обязательное поле',
+  symbols: 'Использованы неподходящие символы',
+  min: 'Минимальное длина - 2',
+  format: 'Неправильный формат'
+};
+
+const regExSymb = /[^А-ЯЁа-яёA-Za-z\-]/;
+const regExName = /^[А-ЯЁа-яё]+(\-[А-ЯЁа-яё]{2,})?$|^[A-Za-z]+(\-[A-Za-z]{2,})?$/;
+
+let forActivationButton = {
+  name: false,
+  surname: false,
+  application: false,
+  cv: false,
+  video: false,
+};
+// ==============================
+
+
+
 
 // Функция скролла к верху элемента 
 function scrollTo(element) {
   element.scrollIntoView({ behavior: 'smooth' });
 }
 
+// Функция проверка на ошибку и смена цвета границы
+function changeColorBorder(text, block) {
+  if (text === 'reset') {
+    block.classList.remove('popup__field-input_success');
+    block.classList.remove('popup__field-input_failed');
+  } else
+  if (text) {
+    block.classList.add('popup__field-input_failed');
+  } else {
+    block.classList.remove('popup__field-input_failed');
+    block.classList.add('popup__field-input_success');
+  }
+}
+
+// Функция добавления обработчиков событий на поле ввода
+function addListenerToField(field, key) {
+
+  const fieldErrorBlock = field.querySelector('.popup__field-error');
+  const fieldInputBlock = field.querySelector('.popup__field-input');
+
+  function changeData(message, boolean) {
+    fieldErrorBlock.textContent = message;
+    changeColorBorder(fieldErrorBlock.textContent, fieldInputBlock);
+    forActivationButton[key] = boolean;
+
+    deActivateButton();
+  }
+  
+  field.addEventListener('input', (evt) => {
+
+    if (evt.target.value.length < 1) {
+      changeData(errorMessages.required, false);
+    } else
+    if (!!evt.target.value.match(regExSymb)) {
+      changeData(errorMessages.symbols, false);
+    } else
+    if (evt.target.value.length < 2) {
+      changeData(errorMessages.min, false);
+    } else
+    if (!evt.target.value.match(regExName)) {
+      changeData(errorMessages.format, false);
+    } else {
+      let text = fieldInputBlock.value;
+      text = text.toLowerCase();
+      text = text[0].toUpperCase() + text.slice(1);
+      
+      if (text.indexOf('-')) {
+        text = text.slice(0, text.indexOf('-') + 1) + text[text.indexOf('-') + 1].toUpperCase() + text.slice(text.indexOf('-') + 2);
+      }
+      
+      changeData('', true);
+      fieldInputBlock.value = text;
+    }
+  });
+
+  return { error: fieldErrorBlock, input: fieldInputBlock };
+}
+const fieldNameObject = addListenerToField(popupNameField, 'name');
+const fieldSurnameObject = addListenerToField(popupSurnameField, 'surname');
+
+// Функция добавления обработчика событий на загрузчики файлов
+function addUploadListener(block, key) {
+  block.addEventListener('change', function() {
+    block.classList.add('popup__upload-label_success');
+    this.querySelector('.popup__upload-text').textContent = this.childNodes[1].files[0].name;
+    forActivationButton[key] = true;
+    deActivateButton();
+  });
+}
+
+// Функция очистки текстовых инпутов
+function cleanInputText(fieldObject) {
+  fieldObject.input.value = '';
+  fieldObject.error.textContent = '';
+  changeColorBorder('reset', fieldObject.input);
+}
+
+// Функция очистки загрузочных инпутов
+function cleanInputFile(block, text) {
+  block.querySelector('.popup__upload-input').value = '';
+  block.querySelector('.popup__upload-text').textContent = text;
+  block.classList.remove('popup__upload-label_success');
+}
+
+// Функция закрытия попапа
+function closePopup() {
+  popupBlock.style.display = 'none';
+  cleanInputText(fieldNameObject);
+  cleanInputText(fieldSurnameObject);
+
+  cleanInputFile(popupUploadApplication, 'Загрузить заявку (pdf)');
+  cleanInputFile(popupUploadCV, 'Загрузить резюме (pdf)');
+  cleanInputFile(popupUploadVideo, 'Загрузить видео (mp4, avi, mov)');
+
+  forActivationButton = {
+    name: false,
+    surname: false,
+    application: false,
+    cv: false,
+    video: false,
+  };
+  deActivateButton();
+  document.body.style.overflow = 'auto';
+}
+
+// Функция открытия попапа
+function openPopup() {
+  popupBlock.style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+}
+
+// Функция активации/деактивации кнопки формы
+function deActivateButton() {
+  let counter = 0;
+  for (let el in forActivationButton) {
+    if (!forActivationButton[el]) {
+      ++counter;
+      break;
+    }
+  }
+
+  if (!counter) {
+    popupButton.removeAttribute('disabled');
+    popupButton.classList.remove('popup__form-button_disabled');
+  } else {
+    popupButton.setAttribute('disabled', true);
+    popupButton.classList.add('popup__form-button_disabled');
+  }
+}
+// ==============================
+
+
+
 
 // Обработчик сравнивает текстконтент элемента, на который нажали, и заголовков блоков страницы
+// scroll
 headerNav.addEventListener('click', (e) => {
   if (e.target.classList.contains('header__list-item')) {
-    for (let el of [aboutBlock, participationBlock, timingBlock]) {
+    for (let el of [aboutBlock, participationBlock, timingBlock, hackathonsBlock]) {
       if (el.querySelector('.block-title').textContent === e.target.textContent) {
         scrollTo(el);
         break;
@@ -43,6 +217,22 @@ headerNav.addEventListener('click', (e) => {
 introButton.addEventListener('click', () => { scrollTo(aboutBlock); });
 aboutButton.addEventListener('click', () => { scrollTo(participationBlock); });
 participationButton.addEventListener('click', () => { scrollTo(timingBlock); });
+timingBlock.addEventListener('click', () => { openPopup() });
+
+// Обработчик попапа
+popupBlock.addEventListener('mousedown', (event) => {
+  if (event.target.classList.contains('popup')) {
+    closePopup();
+  }
+});
+
+// Обработчик загрузки файлов
+addUploadListener(popupUploadApplication, 'application');
+addUploadListener(popupUploadCV, 'cv');
+addUploadListener(popupUploadVideo, 'video');
+// ================================
+
+
 
 
 // DVD
